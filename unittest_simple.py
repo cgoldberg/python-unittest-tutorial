186c804d386776b7793509074f4f69f9af7b15a0
Corey Goldberg <cgoldberg@gmail.com>
2013-01-12 12:41:34 -0500

    code sample

---
+++ unittest_simple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
