b8423630081dee47e88d74b6189eb2c291fbc3ac
Corey Goldberg <cgoldberg@gmail.com>
2013-01-13 10:11:24 -0500

    updated code for py3

--- test_fixtures.py
+++ test_fixtures.py
@@ -4,15 +4,15 @@ import unittest
 class FixturesTest(unittest.TestCase):
 
     def setUp(self):
-        print 'In setUp()'
+        print('In setUp()')
         self.fixture = range(1, 10)
 
     def tearDown(self):
-        print 'In tearDown()'
+        print('In tearDown()')
         del self.fixture
 
     def test(self):
-        print 'in test()'
+        print('in test()')
         self.assertEqual(self.fixture, range(1, 10))
 
 if __name__ == '__main__':
